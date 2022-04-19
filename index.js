14b46fa1fb5d93f78d175d1f5bdd30366357f2a5
adhaghani <96929167+adhaghani@users.noreply.github.com>
2022-04-19 15:54:41 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -7,65 +7,6 @@ menulist = () => {
   menu.classList.toggle("active");
 }
 
-window.addEventListener("load", function() {
-  const first = document.getElementById("one")
-  const second = document.getElementById("two")
-  const third = document.getElementById("three")
-  const popup = document.getElementById("popups")
-
-
-
-setTimeout(() => {
-
-    first.style.display="grid";
-    first.classList.toggle("active");
-  
-  setTimeout(() => {
-    first.style.opacity="0";
-  }, 1500);
-}, 1);
-
-setTimeout(() => {
-  first.style.display="none";
-    second.style.display="grid";
-  
-    setTimeout(() => {
-      second.classList.toggle("active")
-    }, 500);
-  
-  setTimeout(() => {
-    second.style.opacity="0";
-  }, 2500);
-}, 2400);
-
-setTimeout(() => {
-  second.style.display="none";
-    third.style.display="grid";
-  
-    setTimeout(() => {
-      third.classList.toggle("active")
-    }, 500);
-  
-  setTimeout(() => {
-    third.style.opacity="0";
-  }, 3000);
-}, 6000);
-
-setTimeout(() => {
-    third.style.display = "none"
-    setTimeout(() => {
-      popup.style.opacity="0";
-
-      setTimeout(() => {
-        popup.style.display="none"
-      }, 401);
-    }, 400);
-}, 10000);
-
-
-})
-
-
 
 // Darkmode localstorage
 
@@ -73,6 +14,7 @@ let darkMode = localStorage.getItem('darkMode'); //cookie of 5 MB (no concern of
 const darkModeToggle = document.getElementById("toggle-dark-mode");
 const moon = document.getElementById("moon")
 const sun = document.getElementById("sun")
+
 
 
 //check if dark mode is enabled
@@ -110,3 +52,167 @@ darkModeToggle.addEventListener('click', () => {
     disableDarkMode();
   }
 });
+
+
+
+
+const popup = document.getElementById("popups")
+const first = document.getElementById("one")
+  const second = document.getElementById("two")
+  const third = document.getElementById("three")
+
+  let popupscreen = localStorage.getItem('pop', 'enabled');
+
+  const enablepop = () => {
+    console.log(popupscreen)
+    setTimeout(() => {
+      setTimeout(() => {
+    
+        first.style.display="grid";
+        first.classList.toggle("active");
+      
+      setTimeout(() => {
+        first.style.opacity="0";
+      }, 1500);
+    }, 1);
+    
+    setTimeout(() => {
+      first.style.display="none";
+        second.style.display="grid";
+      
+        setTimeout(() => {
+          second.classList.toggle("active")
+        }, 500);
+      
+      setTimeout(() => {
+        second.style.opacity="0";
+      }, 2500);
+    }, 2400);
+    
+    setTimeout(() => {
+      second.style.display="none";
+        third.style.display="grid";
+      
+        setTimeout(() => {
+          third.classList.toggle("active")
+        }, 500);
+      
+      setTimeout(() => {
+        third.style.opacity="0";
+      }, 3000);
+    }, 6000);
+    
+    setTimeout(() => {
+        third.style.display = "none"
+        setTimeout(() => {
+          popup.style.opacity="0";
+    
+          setTimeout(() => {
+            popup.style.display="none"
+            localStorage.setItem('popupscreen', null);
+            console.log(popupscreen)
+          }, 401);
+        }, 400);
+    }, 10000);
+    }, 1);
+  }
+  const disablepop = () => {
+    popup.style.display="none"
+  }
+
+  window.addEventListener("load", () => {
+    popupscreen = localStorage.getItem('popupscreen');
+    if (popupscreen == 'enabled'){
+      enablepop()
+    }
+    else {
+      disablepop()
+    }
+  })
+
+  const us = document.getElementById("us");
+  const her = document.getElementById("her");
+  const usbutton = document.getElementById("usbutton");
+  const herbutton = document.getElementById("herbutton");
+  const preloader = document.getElementById("preloader")
+
+  const herclick = () => {
+    usbutton.classList.remove("active")
+    herbutton.classList.add("active")
+      setTimeout(() => {
+        preloader.style.display="grid"
+        setTimeout(() => {
+          preloader.style.opacity="1"
+        }, 100);
+        setTimeout(() => {
+          document.documentElement.scrollTop = 0;
+          document.body.scrollTop = 0;
+        }, 1000);
+        setTimeout(() => {
+          her.style.display="block";
+          us.style.display="none";
+        }, 3900);
+        setTimeout(() => {
+          preloader.style.opacity="0"
+        }, 4000);
+        setTimeout(() => {
+          preloader.style.display="none"
+        }, 4500);
+      }, 1);
+
+  }
+
+  const usclick = () => {
+    usbutton.classList.add("active")
+    herbutton.classList.remove("active")
+
+      setTimeout(() => {
+        preloader.style.display="grid"
+        setTimeout(() => {
+          preloader.style.opacity="1"
+        }, 100);
+        setTimeout(() => {
+          document.documentElement.scrollTop = 0;
+          document.body.scrollTop = 0;
+        }, 1000);
+        setTimeout(() => {
+          us.style.display="block";
+          her.style.display="none";
+
+        }, 3900);
+        setTimeout(() => {
+          preloader.style.opacity="0"
+        }, 4000);
+        setTimeout(() => {
+          preloader.style.display="none"
+        }, 4500);
+      }, 1);
+
+  }
+
+  const herclick2 = () => {
+
+  }
+
+  const usclick2 = () => {
+
+  }
+  
+  usbutton.addEventListener("click",function(){
+
+    if(usbutton.classList.contains("active")) {
+      usclick2();
+    }
+    else {
+      usclick()
+    }
+  })
+
+  herbutton.addEventListener("click",function(){
+    if(herbutton.classList.contains("active")) {
+      herclick2();
+    }
+    else {
+    herclick();
+    }
+  })
